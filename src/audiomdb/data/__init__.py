a8efcc232943dfeb905c8cf628770e11cfd23787
odunola <jenrola2292@gmail.com>
2025-10-01 13:20:00 +0100

    Add CombinedDataset

--- src/audiomdb/data/__init__.py
+++ src/audiomdb/data/__init__.py
@@ -1,4 +1,5 @@
-from audiomdb.data.base import StreamingDataset
+from audiomdb.data.base import StreamingDataset, C
+
 from audiomdb.retrievers.hf import HFDataRetriever
 from audiomdb.retrievers.s3 import S3DataRetriever
 from audiomdb.retrievers.local import LocalDataRetriever
